2665bffebd2dc214e72ed5e9d5eecb91ed257baa
aminejoker <60425674+aminejoker@users.noreply.github.com>
2020-01-29 10:58:41 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("656494031232040970")
+setInterval(function() {
+channel.send(`amine amine amine `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
